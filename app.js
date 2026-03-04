76cf2db5b1882cce9ceea9cfed98a680e2e40429
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-04 09:59:16 +0000

    Subscription API test

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 import express from 'express'
 
 const app = express();
+
+
+// path and callback function in 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Subscription Tracker API!');
+});
+
+
+
+// listening port
+app.listen(3000, () => {
+  console.log('Subscription Tracker API is running on http://localhost:3000');
+});
+
+
+export default app;
