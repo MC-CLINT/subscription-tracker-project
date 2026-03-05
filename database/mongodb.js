458b358cf1cc93c3e5d09b485cf8e8561b9664f7
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-05 14:59:41 +0000

    db connect

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { DB_URI, NODE_ENV } from "../config/env";
+import { DB_URI, NODE_ENV } from "../config/env.js";
 
 
 if(!DB_URI){
@@ -11,7 +11,7 @@ if(!DB_URI){
 const connectToDatabase = async () => {
     try {
         await mongoose.connect(DB_URI);
-        console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB in", NODE_ENV, "mode");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
         process.exit(1);
