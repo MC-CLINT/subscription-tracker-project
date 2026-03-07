e64e51868ebea2a6fbb95e7acd2f7a8364068002
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-07 16:12:15 +0000

    check existing user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,24 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import User from '../models/user.model.js';
 
 
+
+// what is a req.body --> req.body is an object conaining data from the client (POST REQUEST)
 export const signUp = async (req, res, next) => {
     const session = mongoose.startSession();
     session.startTransaction();
 
     try{
+        const {name, email, password} = req.body;
 
+        //checks if  a user already exists
+        const existingUser = await User.findOne({email})
+
+        if(existingUser) {
+            const error = new Error('User already exists');
+            error.statusCode = 409;
+            throw error;
+        }
 
 
 
