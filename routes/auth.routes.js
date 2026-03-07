86f286b442367fc992185f4fd63c1bdde7d4d8fc
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-07 13:56:04 +0000

    route update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import authController from '../controllers/auth.controller.js';
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();  
 
