37a73e4c73091e67433edab5aa54c99ee214ac6d
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-07 13:29:49 +0000

    JWT AUTH

--- config/env.js
+++ config/env.js
@@ -2,8 +2,10 @@ import {config} from 'dotenv';
 
 config({path: `./.env.${process.env.NODE_ENV || 'development'}.local`});
 
-export const NODE_ENV = process.env.NODE_ENV || 'development';
-export const PORT = Number.parseInt(process.env.PORT, 10) || 3000;
-export const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/subscription-tracker';
+export const{
+    PORT, NODE_ENV,
+    DB_URI,
+    JWT_SECRET, JWT_EXPIRES_IN
+} = process.env;
 
 
