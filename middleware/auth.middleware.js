eea220643be928d06ab20ce3640afdedd0c1838c
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-08 10:47:34 +0000

    authorization

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,13 @@
 const authorize = async (req, res, next) => {
-    
+    try{
+        let 
+
+    }catch(error){
+
+        // 401 stands for unauthorised
+        res.status(401).json({ message: "unauthorized", error: "error.message"})
+    }
+
+
+
 }
