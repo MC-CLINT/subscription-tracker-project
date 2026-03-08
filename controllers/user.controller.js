0934c151293159c377d7ae84de5d2dc6880f44eb
MC-CLINT <ckvidam@st.ug.edu.gh>
2026-03-08 10:36:50 +0000

    auth middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,8 +20,6 @@ export const getUser = async(req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
-
-
         res.status(200).json({success: true, data: user});    
     }catch(error){
         next(error)
